d2d1f1a306dda1b49cb0c84d2e03b84ab7227f89
Dmitrii-Q <120948230+Dmitrii-Q@users.noreply.github.com>
2023-05-18 12:18:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+  public static void main(String[] args) {
+  
+    // Объявляете переменные для входных данных и
+    // параметров программы: начального счёта, 
+    // суммы пополнения и тп
+    
+    // Условным оператором проверяете, превысила ли
+    // сумма пополнения порог, и для этих двух разных
+    // сценариев рассчитываете сумму бонуса и выводите
+    // на экран.
+  }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 100;
+        int addend = 1200;
+
+        int bonus = 0;
+        if (addend > 1000) {
+            bonus = addend / 100;
+        }
+        int total = sum + addend + bonus;
+
+        System.out.println("Итоговый счет - " + total);
+        System.out.println("Бонусная часть - " + bonus);
+    }
+}
+Итоговый счет - 1312
+Бонусная часть - 12
+
+Process finished with exit code 0
